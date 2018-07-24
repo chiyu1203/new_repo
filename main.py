b509ebfe02e64b8f64c15cf1750dba8414a99c07
Chi Yu <ucqfcle@ucl.ac.uk>
2018-07-24 10:52:46 +0100

    added Dog class

--- main.py
+++ main.py
@@ -1,1 +1,13 @@
-print('This is our script')
+print('This is our script')import numpy as np
+
+class Dog:
+	def __init__(self):
+		self.n_paws = 4
+		self.n_ears = 2
+
+	def bark(self):
+		print('Woof')
+
+	def get_number_of_paws(self):
+		return self.n_paws
+
